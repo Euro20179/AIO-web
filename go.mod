43453153909bb2bd1a01dd42e3582b4383faaf52
anon <anon>
2025-04-17 19:03:50 -0700

    refactor: make hostnames generic and create server config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module aio-web
 
 go 1.24.2
+
+require gopkg.in/ini.v1 v1.67.0 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	// "aiolimas/webservice/dynamic"
+	"fmt"
+	"html/template"
 	"net/http"
+	"os"
 	"path/filepath"
+	"strings"
+
+	"gopkg.in/ini.v1"
 	// "strings"
 )
+
+var config *ini.File;
+
+type TemplateInfo struct {
+	AIO string
+	API string
+}
 
 func root(w http.ResponseWriter, req *http.Request) {
 	rootPath := "./www"
@@ -15,10 +28,39 @@ func root(w http.ResponseWriter, req *http.Request) {
 	// 	return
 	// }
 	fullPath := filepath.Join(rootPath, path)
-	http.ServeFile(w, req, fullPath)
+
+	if strings.HasSuffix(fullPath, ".html") {
+		tmpl, err := template.ParseFiles(fullPath)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("Failed to parse file as template"))
+			return;
+		}
+
+		aioLimasSection, _ := config.GetSection("aio_limas")
+		host, err := aioLimasSection.GetKey("host")
+		hostString := "http://localhost:8080"
+		if err == nil {
+			hostString = host.String()
+		} else {
+			fmt.Fprintf(os.Stderr, "Failed to get config host: %s", err.Error())
+		}
+
+
+		tmpl.Execute(w, TemplateInfo { AIO: hostString})
+	} else {
+		http.ServeFile(w, req, fullPath)
+	}
 }
 
 func main() {
+	data, err := ini.Load("./server-config.ini")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to load config\n%s", err.Error()))
+	}
+
+	config = data
+
 	http.HandleFunc("/", root)
 	http.ListenAndServe(":8081", nil)
 }
