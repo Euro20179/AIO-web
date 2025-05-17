d52898ea35d6766bd1197ea414e909f16504d87d
euro20179 <sandollar2020@gmail.com>
2025-05-17 07:39:12 -0700

    fix: scriptMode not tracked

---
+++ www/ui/view-modes/scriptMode.ts
@@ -0,0 +1,44 @@
+const run = document.getElementById("script-execute") as HTMLButtonElement
+const scriptBox = document.getElementById("script") as HTMLTextAreaElement
+const scriptOutput = document.getElementById("script-execute-output") as HTMLDivElement
+
+const modeScripting: DisplayMode = {
+    add(entry) {
+        console.log(mode, mode === modeScripting, entry)
+        if (mode !== modeScripting) return
+        renderDisplayItem(entry.ItemId, scriptOutput)
+    },
+    sub(entry) {
+        if (mode !== modeScripting) return
+        scriptOutput.querySelector(`[data-item-id="${entry.ItemId}"]`)?.remove()
+    },
+    addList(entries) {
+        if (mode !== modeScripting) return
+        for (let e of entries) {
+            this.sub(e)
+        }
+    },
+    subList(entries) {
+        if (mode !== modeScripting) return
+        for (let e of entries) {
+            this.sub(e)
+        }
+    },
+
+    clear() {
+        clearItems()
+        scriptOutput.innerHTML = ""
+    },
+
+    put(html: string) {
+        scriptOutput.innerHTML += html
+    }
+}
+
+run.onclick = function() {
+    const script = scriptBox.value
+
+    console.log(script)
+    const value = parseExpression(script, new SymbolTable)
+    scriptOutput.append(value.jsStr())
+}
