d06d215296dafee51fe56e3734555f56006b99ed
euro20179 <sandollar2020@gmail.com>
2025-06-05 23:49:47 -0700

    feat(mode/events): add resuming variable to filter

--- www/ui/view-modes/event-mode.ts
+++ www/ui/view-modes/event-mode.ts
@@ -81,7 +81,7 @@ function _filterEvents(script: string) {
 
     for (let event of eventOrder.list()) {
         let tbl = makeSymbolsTableFromObj(event)
-        for (let status of ["Planned", "Viewing", "Finished", "Dropped", "Paused", "ReViewing", "Waiting"]) {
+        for (let status of ["Planned", "Viewing", "Finished", "Dropped", "Paused", "ReViewing", "Waiting", "Resuming"]) {
             let is = event.Event === status
             tbl.set(status.toLowerCase(), new Num(Number(is)))
         }
