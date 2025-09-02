7847cf9c22a3f9775cafeeea17c0861729094a10
euro20179 <sandollar2020@gmail.com>
2025-09-02 12:23:34 -0700

    feat(mode/calendar): abilty to render multiple months

--- www/ui/view-modes/calendar.ts
+++ www/ui/view-modes/calendar.ts
@@ -13,17 +13,23 @@ class CalendarMode extends Mode {
         super(parent || "#calendar-output", win)
 
         this.win.document.getElementById("calendar-output")?.classList.add("open")
-        this.canv = this.win.document.getElementById("cal-canvas") as HTMLCanvasElement
-        this.ctx = this.canv.getContext("2d") as CanvasRenderingContext2D
-
-        this.onResize()
-        this.win.addEventListener("resize", this.onResize.bind(this))
 
         this.mode = "month"
 
         this.selectedItems = []
 
-        this.selectedTime = [new Date(Date.now()), new Date(Date.now())]
+        const now = new Date(Date.now())
+        this.selectedTime = [new Date(now.getFullYear(), now.getMonth()), new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59)]
+
+        this._setupWin()
+    }
+
+    _setupWin() {
+        this.canv = this.win.document.getElementById("cal-canvas") as HTMLCanvasElement
+        this.ctx = this.canv.getContext("2d") as CanvasRenderingContext2D
+
+        this.onResize()
+        this.win.addEventListener("resize", this.onResize.bind(this))
 
         this.parent.querySelector("#month-prev")?.addEventListener("click", e => {
             let prevMonth = this.selectedTime[0].getMonth() - 1
@@ -32,7 +38,8 @@ class CalendarMode extends Mode {
                 prevMonth = 11
                 year -= 1
             }
-            this.selectedTime[0] = new Date(year, prevMonth)
+            this._setStart(new Date(year, prevMonth))
+            this._setEnd(new Date(year, prevMonth + 1, 0, 23, 59, 59))
             this._render()
         })
 
@@ -43,9 +50,35 @@ class CalendarMode extends Mode {
                 nextMonth = 0
                 year += 1
             }
-            this.selectedTime[0] = new Date(year, nextMonth)
+            this._setStart(new Date(year, nextMonth))
+            this._setEnd(new Date(year, nextMonth + 1, 0, 23, 59, 59))
             this._render()
         })
+
+        this.parent.querySelector("#time-start")?.addEventListener("change", e => {
+            this._setStart(new Date(e.target.value))
+            this._render()
+        })
+
+        this.parent.querySelector("#time-end")?.addEventListener("change", e => {
+            this._setEnd(new Date(e.target.value))
+            this._render()
+        })
+
+
+        this._setStart(this.selectedTime[0])
+        this._setEnd(this.selectedTime[1])
+    }
+
+    _setStart(date: Date) {
+        this.selectedTime[0] = date
+        const startIn = this.parent.querySelector("#time-start") as HTMLInputElement
+        startIn.value = date.toISOString().slice(0, 10)
+    }
+    _setEnd(date: Date) {
+        this.selectedTime[1] = date
+        const endIn = this.parent.querySelector("#time-end") as HTMLInputElement
+        endIn.value = date.toISOString().slice(0, 10)
     }
 
     get mode() {
@@ -59,8 +92,8 @@ class CalendarMode extends Mode {
     }
 
     onResize() {
-        this.canv.width = this.canv.parentElement?.clientWidth
-        this.canv.height = this.canv.parentElement?.clientHeight
+        this.canv.width = this.canv.parentElement?.clientWidth || 100
+        this.canv.height = this.canv.parentElement?.clientHeight || 100
     }
 
     close() {
@@ -72,28 +105,19 @@ class CalendarMode extends Mode {
         const timeDiffMins = (this.selectedTime[1].getTime() - this.selectedTime[0].getTime()) / 1000 / 60
     }
 
-    _renderMonth() {
-        const output = this.parent.querySelector("#month-display") as HTMLElement | null
-        if (!output) return
+    _renderMonth(start: Date, end?: Date) {
+        end ??= new Date(start.getFullYear(), start.getMonth() + 1, 0)
 
-        const monthGrid = output.querySelector("#month-grid") as HTMLElement
-        const monthName = output.querySelector("#month-name")
-
-        const start = new Date(this.selectedTime[0].getFullYear(), this.selectedTime[0].getMonth(), 1)
-        if (monthName)
-            monthName.innerHTML = `${start.toLocaleString('default', { month: "long" })} ${start.getFullYear()}`
-
-        //the true total number of events
-        let yearEvents = items_getEventsWithinTimeRange(this.selectedItems, new Date(start.getFullYear(), 0, 0).getTime(), new Date(start.getFullYear(), 12, 31, 11, 59, 59).getTime())
-        const totalYearEvents = yearEvents.toArray().length
+        const monthGrid = document.createElement("div")
+        monthGrid.classList.add('month-grid')
 
         //events within the month
         //(finding valid events each day is COSTLY)
         let validEvents: UserEvent[] = [
             ...items_getEventsWithinTimeRange(
                 this.selectedItems,
-                (new Date(start.getFullYear(), start.getMonth(), 1, 0, 0, 0)).getTime(),
-                (new Date(start.getFullYear(), start.getMonth(), 31, 23, 59, 59)).getTime()
+                start.getTime(),
+                end.getTime(),
             )
         ]
 
@@ -104,25 +128,21 @@ class CalendarMode extends Mode {
             monthGrid.removeChild(monthGrid.firstElementChild)
         }
 
-        const month2dayCount = (month: number, year: number) => {
-            return new Date(year, month, 0).getDate()
-        }
-
         for (let i = 0; i < start.getDay(); i++) {
             monthGrid.appendChild(document.createElement("div"))
         }
 
-        for (let day = 1; day <= month2dayCount(start.getMonth() + 1, start.getFullYear()); day++) {
+        let startDate = this.selectedTime[0].getDate()
+        for (let i = 1; i < startDate; i++) {
+            monthGrid.appendChild(document.createElement("div"))
+        }
+
+        for (let day = startDate; day <= end.getDate(); day++) {
             const d = document.createElement("div")
             d.classList.add("day")
             d.setAttribute("data-day", String(day))
             d.append(String(day))
             monthGrid.appendChild(d)
-        }
-
-        const monthStats = {
-            eventCounts: {} as Record<string, number>,
-            totalEvents: 0
         }
 
         const eventTotalsTable = this.parent.querySelector("#event-totals")
@@ -136,13 +156,6 @@ class CalendarMode extends Mode {
             //it's possible that the event may be invalid (validity assumes a month has 31 days)
             if (!d) continue
 
-            monthStats.totalEvents++
-            if (monthStats.eventCounts[ev.Event]) {
-                monthStats.eventCounts[ev.Event] += 1
-            } else {
-                monthStats.eventCounts[ev.Event] = 1
-            }
-
             const item = findInfoEntryById(ev.ItemId)
 
             const eventMarker = document.createElement("span")
@@ -152,29 +165,45 @@ class CalendarMode extends Mode {
             eventMarker.title = items_eventTSText(ev)
             d.appendChild(eventMarker)
         }
+        const monthLabel = document.createElement("h2")
+        monthLabel.innerHTML = `${start.toLocaleString('default', { month: "long" })} ${start.getFullYear()}`
+        monthLabel.classList.add("month-label")
 
-        const totalEvents = this.parent.querySelector("#total-events")
-        if (totalEvents && totalEvents instanceof this.win.HTMLElement) {
-            totalEvents.innerHTML = `${monthStats.totalEvents}<BR>(${(monthStats.totalEvents / totalYearEvents * 100).toFixed(2)}% of year)`
-        }
-
-        if (eventTotalsTable) {
-            const sorted = Object.entries(monthStats.eventCounts).sort((a, b) => b[1] - a[1])
-            for (let [event, count] of sorted) {
-                eventTotalsTable.innerHTML += `<tr>
-    <td>${event}</td>
-    <td>${count}</td>
-    <td>${(count / monthStats.totalEvents * 100).toFixed(2)}</td>
-</tr>`
-            }
-        }
+        return [monthLabel, monthGrid]
     }
 
     _render() {
         if (this.mode === "day") {
             this._renderDay()
         } else {
-            this._renderMonth()
+            const output = this.parent.querySelector("#month-display") as HTMLElement | null
+            if (!output) return
+
+            const months = output.querySelector("#months") as HTMLDivElement
+            while (months.firstElementChild) {
+                months.firstElementChild.remove()
+            }
+
+            if (this.selectedTime[0].getFullYear() == this.selectedTime[1].getFullYear() && this.selectedTime[0].getMonth() === this.selectedTime[1].getMonth()) {
+                for (let child of this._renderMonth(this.selectedTime[0], this.selectedTime[1]))
+                    months.appendChild(child)
+            } else {
+                for (let child of this._renderMonth(this.selectedTime[0]))
+                    months.appendChild(child)
+
+                let currentDate = new Date(this.selectedTime[0].getFullYear(), this.selectedTime[0].getMonth() + 1)
+                while (currentDate.getMonth() < this.selectedTime[1].getMonth() || currentDate.getFullYear() < this.selectedTime[1].getFullYear()) {
+                    for (let child of this._renderMonth(currentDate))
+                        months.appendChild(child)
+                    currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1)
+                }
+
+                if (currentDate.getTime() < this.selectedTime[1].getTime()) {
+                    for (let child of this._renderMonth(currentDate, this.selectedTime[1])) {
+                        months.appendChild(child)
+                    }
+                }
+            }
         }
     }
 
@@ -208,9 +237,6 @@ class CalendarMode extends Mode {
         this.win.close()
         this.win = win
         this.parent = win.document.getElementById("calendar-output") as HTMLCanvasElement
-        this.canv = this.win.document.getElementById("cal-canvas") as HTMLCanvasElement
-        this.ctx = this.canv.getContext("2d") as CanvasRenderingContext2D
-        this.win.addEventListener("resize", this.onResize.bind(this))
-        this._render()
+        this._setupWin()
     }
 }
