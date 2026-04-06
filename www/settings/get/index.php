2f92033fe4f1f5a972f0fc496535f8d0a07e0db6
euro20179 <Euro20171@proton.me>
2026-04-06 13:23:10 -0700

    feat(settings): must be logged in to access settings

--- www/settings/get/index.php
+++ www/settings/get/index.php
@@ -1,6 +1,23 @@
 <?php
 include "../../lib/util.php";
 
+if (array_key_exists("auth", $_GET)) {
+    $auth = $_GET["auth"];
+} else if (array_key_exists("HTTP_AUTHORIZATION", $_SERVER)) {
+    $auth = $_SERVER["HTTP_AUTHORIZATION"];
+    $auth = substr($auth, 6);
+} else {
+    http_response_code(401);
+    exit();
+}
+
+error_log("auth");
+error_log($auth);
+if (!ckauth($auth)) {
+    http_response_code(401);
+    exit();
+}
+
 if (!array_key_exists("uid", $_GET)) {
     echo "Expected a ?uid to look up the settings for";
     http_response_code(400);
